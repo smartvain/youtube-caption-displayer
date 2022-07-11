3d8e0135ee50da5eba25cb5408b3822d4da63a60
Smart Vain <zlc.blue15210@gmail.com>
2022-07-12 07:50:00 +0900

    add: normal test of getLangList()

--- src/Tests/DisplayerTest.php
+++ src/Tests/DisplayerTest.php
@@ -10,20 +10,51 @@ use Tests\TestCase;
 class DisplayerTest extends TestCase
 {
     /**
-     * Test to get lang list
+     * @var string
+     */
+    private static string $caption_exist_url = 'https://www.youtube.com/watch?v=zOjov-2OZ0E&t=7s';
+
+    /**
+     * @var string
+     */
+    private static string $caption_not_exist_url = 'https://www.youtube.com/watch?v=WFsAon_TWPQ';
+    
+    /**
+     * Test to count lang list.
      *
      * @return void
      */
-    public function testGetLangList()
+    public function testCountLangList()
     {
-        // $lang_list = Displayer::getLangList('https://www.youtube.com/watch?v=zOjov-2OZ0E&t=7s');
-        $lang_list = Displayer::getLangList('https://www.youtube.com/watch?v=mZ23BLKnA1Y');
+        $lang_list = Displayer::getLangList(self::$caption_exist_url);
 
         $this->assertTrue(count($lang_list) > 0);
+    }
+
+    /**
+     * Test if lang list has particular keys.
+     *
+     * @return void
+     */
+    public function testLangListHasKey()
+    {
+        $lang_list = Displayer::getLangList(self::$caption_exist_url);
 
         $lang_list->each(function ($lang) {
             $this->assertArrayHasKey('text', $lang);
             $this->assertArrayHasKey('code', $lang);
         });
+    }
+
+    /**
+     * Test when lang list not exist.
+     *
+     * @return void
+     */
+    public function testLangListNotExist()
+    {
+        $lang_list = Displayer::getLangList(self::$caption_not_exist_url);
+
+        $this->assertTrue(count($lang_list) === 0);
     }
 }
